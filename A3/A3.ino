f2829219a6214b85c2a2e97675432ae6a12bede4
annoyingpart <j.hamborsky31@gmail.com>
2024-05-12 14:41:13 +0200

    Add new functionality to unlock A4 module
    Added new library ESP8266HTTPClient for HTTP requests. An unlock function for the A4 module was created, and will execute if the A4 module is inactive. Additional parameters, such as server address, were also set up to aid the unlock function.

--- A3/A3.ino
+++ A3/A3.ino
@@ -1,10 +1,14 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
 
 const char* ssid = "OSK_5261";
 const char* password = "KLJ1RXA6S0";
+const char* serverAddressA4module = "192.168.100.251";
 
 int controlPin = D1;
+
+bool isActiveA4 = false;
 
 ESP8266WebServer server(80);
 
@@ -40,12 +44,44 @@ void setUpRoutes() {
   server.on("/unlock", HTTP_POST, []() {
     digitalWrite(controlPin, HIGH);
     server.send(200, "text/plain", "Message received");
+    if (isActiveA4 == false) {
+      unlockA4module();
+    }
   });
 
   server.on("/lock", HTTP_POST, []() {
     digitalWrite(controlPin, LOW);
     server.send(200, "text/plain", "Message received");
   });
+}
+
+void unlockA4module() {
+  HTTPClient http;
+  WiFiClient client;
+  String url = "http://" + String(serverAddressA4module) + "/unlock_game";
+
+  // Begin HTTP request with WiFi client
+  http.begin(client, url);
+
+  // Add headers
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int httpResponseCode = http.POST("");
+  Serial.println("POST: " + url);
+
+  // Check response
+  if (httpResponseCode > 0) {
+    Serial.print("Message sent successfully. Server response code: ");
+    Serial.println(httpResponseCode);
+    isActiveA4 = true;
+    String response = http.getString();
+    Serial.println("Server response: " + response);
+  } else {
+    Serial.print("Error sending message. Error code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  // End HTTP request
+  http.end();
 }
 
 void startServer() {
